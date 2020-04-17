7fec4a66ad76afc813e86939b3ae4ef99f5cd2d3
Roy Phillips <aroyphillips@gmail.com>
2020-04-17 16:28:41 -0500

    "Fixed percent formatting & minor typos"

--- run_this.py
+++ run_this.py
@@ -18,25 +18,25 @@ class FireCLI():
         print("Assuming hotels house " + str(people_per_room) +
               " people per room, use " + str(num_employees_per_10_rooms) +
               " employees per 10 rooms used for the homeless, "
-              "and inflate the miniumum wage for thier state by "
+              "and inflate the minimum wage for their state by "
               + str(min_wage_inflation_percentage * 100 )+ "% for each employee: \n")
         
-        print("The total duratoinal cost and "
+        print("The total durational cost and "
               "percent of the passed 2 trillion dollar stimulus bill would be as follows: \n")
         
-        print("Housing for 1 Night Cost: ${:,.2f} --> Percentage of stimulus bill {}%"
+        print("Housing for 1 Night Cost: ${:,.2f} --> Percentage of stimulus bill {:3.4f}%"
               .format(sum_1, sum_1/2000000000000*100.0))
 
-        print("Housing for 15 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {}%"
+        print("Housing for 15 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {:3.4f}%"
               .format(sum_15, sum_15 / 2000000000000 * 100.0))
 
-        print("Housing for 30 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {}%"
+        print("Housing for 30 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {:3.4f}%"
               .format(sum_30, sum_30 / 2000000000000 * 100.0))
 
-        print("Housing for 45 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {}%"
+        print("Housing for 45 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {:3.4f}%"
               .format(sum_45, sum_45 / 2000000000000 * 100.0))
 
-        print("Housing for 60 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {}%"
+        print("Housing for 60 Nights Cost: ${:,.2f} --> Percentage of stimulus bill {:3.4f}%"
               .format(sum_60, sum_60 / 2000000000000 * 100.0))
 
         print("\n---------------------------------------------------------------------------------------------\n")
